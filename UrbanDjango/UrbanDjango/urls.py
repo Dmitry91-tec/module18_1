13a1aa428378dd3798803c221297a28fd08d5d44
Dmitry91-tec <nasdi91@yandex.ru>
2024-12-30 01:19:44 +1000

    Create task3

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from task2.views import func_template, class_template
+#from task2.views import func_template, class_template
+from task3.views import func_template, class_template, class_template_cart
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', func_template),
-    path('class', class_template.as_view())
+    path('platform/', func_template),
+    path('platform/games/', class_template.as_view()),
+    path('platform/cart/', class_template_cart.as_view())
 ]
